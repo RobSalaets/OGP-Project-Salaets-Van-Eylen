42d69887319d87287d9ea9be5d7745b9e3de96fe
Rob <Rob@192.168.1.119>
2017-05-04 21:29:09 +0200

    Rob ander pc test

--- OGP1617-Asteroids/src-provided/asteroids/part2/Part2.java
+++ OGP1617-Asteroids/src-provided/asteroids/part2/Part2.java
@@ -5,13 +5,8 @@ import asteroids.part2.internal.AsteroidsFrame2;
 
 public class Part2 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		boolean tryFullscreen = true;
 		boolean enableSound = true;
-=======
-		boolean tryFullscreen = false;
-		boolean enableSound = false;
->>>>>>> upstream/master
 		for (String arg : args) {
 			if (arg.equals("-fullscreen")) {
 				tryFullscreen = true;
