138e78c548220615fddb545fc7f312a2b5a2856f
Rob <Rob@192.168.1.120>
2017-05-17 21:34:13 +0200

    entity expressions merged

--- OGP1617-Asteroids/src/asteroids/model/programs/expressions/UnaryEntityExpression.java
+++ OGP1617-Asteroids/src/asteroids/model/programs/expressions/UnaryEntityExpression.java
@@ -6,11 +6,12 @@ import asteroids.model.programs.Scope;
 import asteroids.model.programs.expressions.types.DoubleLiteral;
 import asteroids.model.programs.expressions.types.EntityLiteral;
 import asteroids.model.programs.expressions.types.Type;
+import asteroids.part3.programs.SourceLocation;
 
 public class UnaryEntityExpression extends UnaryExpression<EntityLiteral, DoubleLiteral>{
 
-	public UnaryEntityExpression(Expression<? super EntityLiteral> arg, UnaryEntityOperation operation) throws IllegalArgumentException{
-		super(arg);
+	public UnaryEntityExpression(Expression<? super EntityLiteral> arg, UnaryEntityOperation operation, SourceLocation location) throws IllegalArgumentException{
+		super(arg, location);
 		if(operation == null)
 			throw new IllegalArgumentException("The UnaryEntityOperation must be effective.");
 		this.operationType = operation;
@@ -22,7 +23,7 @@ public class UnaryEntityExpression extends UnaryExpression<EntityLiteral, Double
 	public DoubleLiteral evaluate(Scope scope, World world) throws ExpressionEvaluationException{
 		Type arg = getArgument().evaluate(scope, world);
 		if(!((arg instanceof EntityLiteral)))
-			throw new ExpressionEvaluationException("Given operands do not evaluate to EntityLiteral");
+			throw new ExpressionEvaluationException("Given operands do not evaluate to EntityLiteral", getSourceLocation());
 		
 		Double result = null;
 		switch(operationType){
--- OGP1617-Asteroids/src/asteroids/model/programs/expressions/UnaryGetDirExpression.java
+++ OGP1617-Asteroids/src/asteroids/model/programs/expressions/UnaryGetDirExpression.java
@@ -6,18 +6,19 @@ import asteroids.model.programs.Scope;
 import asteroids.model.programs.expressions.types.DoubleLiteral;
 import asteroids.model.programs.expressions.types.ShipEntity;
 import asteroids.model.programs.expressions.types.Type;
+import asteroids.part3.programs.SourceLocation;
 
 public class UnaryGetDirExpression extends UnaryExpression<ShipEntity, DoubleLiteral>{
 	
-	public UnaryGetDirExpression(Expression<? super ShipEntity> arg){
-		super(arg);
+	public UnaryGetDirExpression(Expression<? super ShipEntity> arg, SourceLocation location){
+		super(arg, location);
 	}
 
 	@Override
 	public DoubleLiteral evaluate(Scope scope, World world) throws ExpressionEvaluationException{
 		Type arg = getArgument().evaluate(scope, world);
 		if(!((arg instanceof ShipEntity)))
-			throw new ExpressionEvaluationException("Given operands do not evaluate to ShipEntity");
+			throw new ExpressionEvaluationException("Given operands do not evaluate to ShipEntity", getSourceLocation());
 		return new DoubleLiteral(((Ship)((ShipEntity)arg).getValue()).getOrientation());
 	}
 	
